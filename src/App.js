a33e1f8a6c601c18236809b24fd8949f3d089fa0
EC2 Default User <ec2-user@ip-172-31-30-99.ap-northeast-1.compute.internal>
2023-03-13 08:27:42 +0000

    Changes for v3

--- src/App.js
+++ src/App.js
@@ -4,15 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1>Hello from V2</h1>
-          </header>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Hello from V2</h1>
+        </header>
       </div>
-      </header>
     </div>
   );
 }
